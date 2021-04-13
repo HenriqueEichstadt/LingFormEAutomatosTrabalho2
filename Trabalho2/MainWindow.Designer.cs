90a74b3306892ec45b29cf4dcce0b0cd109c62cb
Henrique Eichstädt <henrique98ti@gmail.com>
2021-04-12 21:46:38 -0300

    Adicionando número na linha do editor

---
+++ Trabalho2/MainWindow.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace Trabalho2
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CampoEditor = new ScintillaNET.Scintilla();
+            this.SuspendLayout();
+            // 
+            // CampoEditor
+            // 
+            this.CampoEditor.Location = new System.Drawing.Point(-1, 1);
+            this.CampoEditor.Name = "CampoEditor";
+            this.CampoEditor.Size = new System.Drawing.Size(885, 300);
+            this.CampoEditor.TabIndex = 0;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.CampoEditor);
+            this.Name = "MainWindow";
+            this.Text = "MainWindow";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ScintillaNET.Scintilla CampoEditor;
+    }
+}
---
+++ Trabalho2/MainWindow.cs
@@ -0,0 +1,54 @@
+﻿using ScintillaNET;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabalho2
+{
+    public partial class MainWindow : Form
+    {
+        /// <summary>
+        /// change this to whatever margin you want the line numbers to show in
+        /// </summary>
+        private const int NUMBER_MARGIN = 1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            // Configurações básicas
+            CampoEditor.TextChanged += OnTextChanged;
+
+            // Adicionando número da linha
+            CarregarEditor();
+        }
+
+        private void CarregarEditor()
+        {
+            var margem = CampoEditor.Margins[NUMBER_MARGIN];
+            margem.Width = 40;
+            margem.Type = MarginType.Number;
+            margem.Sensitive = true;
+            margem.Mask = 0;
+        }
+
+        private void OnTextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public static Color IntToColor(int rgb)
+        {
+            return Color.FromArgb(255, (byte)(rgb >> 16), (byte)(rgb >> 8), (byte)rgb);
+        }
+    }
+}
