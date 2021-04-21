7bf4e5d3072874bbc939d6ad0f5f140c130994cc
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2021-04-21 16:17:49 -0300

    Finalizando item Geral

--- Trabalho2/MainWindow.Designer.cs
+++ Trabalho2/MainWindow.Designer.cs
@@ -30,30 +30,86 @@ namespace Trabalho2
         private void InitializeComponent()
         {
             this.CampoEditor = new ScintillaNET.Scintilla();
+            this.CampoResultado = new System.Windows.Forms.TextBox();
+            this.BotaoAnalisar = new System.Windows.Forms.Button();
+            this.BotaoLimpar = new System.Windows.Forms.Button();
+            this.BotaoEquipe = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // CampoEditor
             // 
             this.CampoEditor.Location = new System.Drawing.Point(-1, 1);
+            this.CampoEditor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CampoEditor.Name = "CampoEditor";
             this.CampoEditor.Size = new System.Drawing.Size(885, 300);
             this.CampoEditor.TabIndex = 0;
             // 
+            // CampoResultado
+            // 
+            this.CampoResultado.Location = new System.Drawing.Point(-1, 340);
+            this.CampoResultado.Margin = new System.Windows.Forms.Padding(4);
+            this.CampoResultado.Multiline = true;
+            this.CampoResultado.Name = "CampoResultado";
+            this.CampoResultado.Size = new System.Drawing.Size(885, 150);
+            this.CampoResultado.TabIndex = 1;
+            // 
+            // BotaoAnalisar
+            // 
+            this.BotaoAnalisar.Location = new System.Drawing.Point(-1, 306);
+            this.BotaoAnalisar.Margin = new System.Windows.Forms.Padding(4);
+            this.BotaoAnalisar.Name = "BotaoAnalisar";
+            this.BotaoAnalisar.Size = new System.Drawing.Size(290, 30);
+            this.BotaoAnalisar.TabIndex = 2;
+            this.BotaoAnalisar.Text = "analisar";
+            this.BotaoAnalisar.UseVisualStyleBackColor = true;
+            // 
+            // BotaoLimpar
+            // 
+            this.BotaoLimpar.Location = new System.Drawing.Point(297, 306);
+            this.BotaoLimpar.Margin = new System.Windows.Forms.Padding(4);
+            this.BotaoLimpar.Name = "BotaoLimpar";
+            this.BotaoLimpar.Size = new System.Drawing.Size(290, 30);
+            this.BotaoLimpar.TabIndex = 3;
+            this.BotaoLimpar.Text = "limpar";
+            this.BotaoLimpar.UseVisualStyleBackColor = true;
+            // 
+            // BotaoEquipe
+            // 
+            this.BotaoEquipe.Location = new System.Drawing.Point(593, 306);
+            this.BotaoEquipe.Margin = new System.Windows.Forms.Padding(4);
+            this.BotaoEquipe.Name = "BotaoEquipe";
+            this.BotaoEquipe.Size = new System.Drawing.Size(290, 30);
+            this.BotaoEquipe.TabIndex = 4;
+            this.BotaoEquipe.Text = "equipe";
+            this.BotaoEquipe.UseVisualStyleBackColor = true;
+            // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.ClientSize = new System.Drawing.Size(882, 491);
+            this.Controls.Add(this.BotaoEquipe);
+            this.Controls.Add(this.BotaoLimpar);
+            this.Controls.Add(this.BotaoAnalisar);
+            this.Controls.Add(this.CampoResultado);
             this.Controls.Add(this.CampoEditor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
             this.Name = "MainWindow";
             this.Text = "MainWindow";
             this.Load += new System.EventHandler(this.MainWindow_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private ScintillaNET.Scintilla CampoEditor;
+        private System.Windows.Forms.TextBox CampoResultado;
+        private System.Windows.Forms.Button BotaoAnalisar;
+        private System.Windows.Forms.Button BotaoLimpar;
+        private System.Windows.Forms.Button BotaoEquipe;
     }
 }
